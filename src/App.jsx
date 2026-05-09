78b85720c21036f0640dfeaf2e2a732495d69500
luca <luca.romano@t-online.de>
2026-05-09 20:49:47 +0200

    feat(request): :construction: use user input to query wikipedia api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import { useState } from "react";
-import "./app.css";
+import "./App.css";
 
 export default function App() {
   const [text, setText] = useState("");
+
+    async function fetchWiki(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        const pages = data.query.pages;
+        const page = Object.values(pages)[0];
+
+        console.log(page.extract);
+    }
+
+    function handleChange(e) {
+        const value = e.target.value;
+        setText(value);
+        const url =
+            `https://de.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&titles=${value}`;
+        fetchWiki(url);
+    }
 
   return (
       <div>
@@ -10,12 +27,12 @@ export default function App() {
             className="NiceInput"
             type="text"
             value={text}
-            onChange={(e) => setText(e.target.value)}
+            onChange={handleChange}
             placeholder="Wikipedia-Suche..."
         />
 
         <div className="NiceOutput">
-          {text}
+          In state: {text}
         </div>
       </div>
   );
